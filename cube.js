16a91efcbb01a839e4a9ee00fd7a303a7f9d56e0
Anton Panchishin <anton.panchishin@gmail.com>
2020-04-14 18:30:06 -0400

    addition of threejs example

--- cube.js
+++ cube.js
@@ -1,26 +1,11 @@
 
+
 var do_animation = false;
 
-var InitDemo = function () {
-    do_animation = false;
-    var vertexShaderText = document.getElementById("vertex-shader").value;
-    var fragmentShaderText = document.getElementById("fragment-shader").value;
-
-    console.log('This is working');
-
+function linkUpWebgl() {
     var canvas = document.getElementById('game-surface');
     var gl = canvas.getContext('webgl');
 
-    if (!gl) {
-        console.log('WebGL not supported, falling back on experimental-webgl');
-        gl = canvas.getContext('experimental-webgl');
-    }
-
-    if (!gl) {
-        alert('Your browser does not support WebGL');
-    }
-
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.CULL_FACE);
@@ -31,10 +16,10 @@ var InitDemo = function () {
     // Create shaders
     // 
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(vertexShader, document.getElementById("vertex-shader").value);
 
-    gl.shaderSource(vertexShader, vertexShaderText);
-    gl.shaderSource(fragmentShader, fragmentShaderText);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, document.getElementById("fragment-shader").value);
 
     gl.compileShader(vertexShader);
     if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
@@ -62,88 +47,23 @@ var InitDemo = function () {
         return;
     }
 
-    //
-    // Create buffer
-    //
-    var boxVertices = 
-    [ // X, Y, Z           R, G, B
-        // Top
-        -1.0, 1.0, -1.0,   0.0, 0.0, 0.0,
-        -1.0, 1.0, 1.0,    0.0, 0.0, 0.0,
-        1.0, 1.0, 1.0,     0.0, 0.0, 0.0,
-        1.0, 1.0, -1.0,    0.0, 0.0, 0.0,
+    return [canvas, gl, program]
+}
 
-        // Left
-        -1.0, 1.0, 1.0,    0.0, 0.0, 0.0,
-        -1.0, -1.0, 1.0,   0.0, 0.0, 0.0,
-        -1.0, -1.0, -1.0,  0.0, 0.0, 0.0,
-        -1.0, 1.0, -1.0,   0.0, 0.0, 0.0,
 
-        // Right
-        1.0, 1.0, 1.0,    0.0, 0.0, 0.0,
-        1.0, -1.0, 1.0,   0.0, 0.0, 0.0,
-        1.0, -1.0, -1.0,  0.0, 0.0, 0.0,
-        1.0, 1.0, -1.0,   0.0, 0.0, 0.0,
+var InitDemo = function () {
 
-        // Front
-        1.0, 1.0, 1.0,    0.0, 0.0, 0.0,
-        1.0, -1.0, 1.0,   0.0, 0.0, 0.0,
-        -1.0, -1.0, 1.0,  0.0, 0.0, 0.0,
-        -1.0, 1.0, 1.0,   0.0, 0.0, 0.0,
+    do_animation = false;
 
-        // Back
-        1.0, 1.0, -1.0,    0.0, 0.0, 0.0,
-        1.0, -1.0, -1.0,   0.0, 0.0, 0.0,
-        -1.0, -1.0, -1.0,  0.0, 0.0, 0.0,
-        -1.0, 1.0, -1.0,   0.0, 0.0, 0.0,
+    var canvas, gl, program;
+    [canvas, gl, program] = linkUpWebgl();
 
-        // Bottom
-        -1.0, -1.0, -1.0,   0.0, 0.0, 0.0,
-        -1.0, -1.0, 1.0,    0.0, 0.0, 0.0,
-        1.0, -1.0, 1.0,     0.0, 0.0, 0.0,
-        1.0, -1.0, -1.0,    0.0, 0.0, 0.0,
-    ];
-    for (var i=0; i<boxVertices.length; i+=3*2*4) {
-        var offset = [ 0.0, 0.0, 0.0,  0.0, 0.0, 0.0,  
-                       0.0, 0.0, 0.0,  0.0, 1.0, 0.0,
-                       0.0, 0.0, 0.0,  1.0, 1.0, 0.0,
-                       0.0, 0.0, 0.0,  1.0, 0.0, 0.0 ];
-        for (var j=0; j<offset.length; j++) {
-            boxVertices[i+j] += offset[j]
-        }
-    }
-
-    var boxIndices =
-    [
-        // Top
-        0, 1, 2,
-        0, 2, 3,
-
-        // Left
-        5, 4, 6,
-        6, 4, 7,
-
-        // Right
-        8, 9, 10,
-        8, 10, 11,
-
-        // Front
-        13, 12, 14,
-        15, 14, 12,
-
-        // Back
-        16, 17, 18,
-        16, 18, 19,
-
-        // Bottom
-        21, 20, 22,
-        22, 20, 23
-    ];
 
     var boxVertexBufferObject = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
 
+    
     var boxIndexBufferObject = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
@@ -186,11 +106,8 @@ var InitDemo = function () {
     mat4.lookAt(viewMatrix, [0, 0, -5], [0, 0, 0], [0, 1, 0]);
     mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.clientWidth / canvas.clientHeight, 0.1, 1000.0);
     
-
-    gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
     gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
     gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
-    gl.uniform1f(matTimeUniformLocation, (Date.now() / 100.) %100.);
 
     var xRotationMatrix = new Float32Array(16);
     var yRotationMatrix = new Float32Array(16);
@@ -204,15 +121,12 @@ var InitDemo = function () {
     do_animation = true;
     var loop = function () {
         if (do_animation) {
-            angle = performance.now() / 3000 / 6 * 2 * Math.PI;
+            angle = performance.now() / 6000 / 6 * 2 * Math.PI;
             mat4.rotate(yRotationMatrix, identityMatrix, angle, [0, 1, 0]);
-            mat4.rotate(xRotationMatrix, identityMatrix, angle / 4, [1, 0, 0]);
+            mat4.rotate(xRotationMatrix, identityMatrix, angle / 13, [1, 0, 0]);
             mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
             gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-            gl.uniform1f(matTimeUniformLocation, (Date.now() / 100.) %100.);
-
-            gl.clearColor(0.0, 0.0, 0.0, 1.0);
-            gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+            gl.uniform1f(matTimeUniformLocation, performance.now() / 100 );
             gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
 
             requestAnimationFrame(loop);
